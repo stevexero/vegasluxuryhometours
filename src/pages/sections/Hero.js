173c3c80e678ba27363c01572b4e85a15e7bf49d
Steven Woodward <steveanthony999@gmail.com>
2021-11-27 21:35:20 -0800

    added hero image and overlay

--- src/pages/sections/Hero.js
+++ src/pages/sections/Hero.js
@@ -1,10 +1,16 @@
 import './Hero.css';
 
+import HeroImg from '../../assets/hero_img.jpeg';
+
 const Hero = () => {
   return (
-    <div className='Hero'>
-      <h1>Hero Section</h1>
-    </div>
+    <header
+      className='Hero'
+      style={{ background: `url(${HeroImg}) no-repeat center center/cover` }}>
+      <div className='overlay'>
+        <h1>Hero Section</h1>
+      </div>
+    </header>
   );
 };
 
