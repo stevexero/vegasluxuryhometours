e3536b144551acfe863792205ac23157387ddde0
Steven Woodward <steveanthony999@gmail.com>
2021-11-27 22:00:39 -0800

    styling nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,28 @@
+import { Link } from 'react-router-dom';
+
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className='Navbar'>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Book</li>
-      </ul>
+      <div className='Navbar-container'>
+        <div className='Navbar-left'>
+          <Link className='rajdhani-bold-24 nav-link' to='/'>
+            Home
+          </Link>
+          <Link className='rajdhani-bold-24 nav-link' to='/about'>
+            About
+          </Link>
+        </div>
+        <div className='Navbar-right'>
+          <Link className='rajdhani-bold-24 nav-link' to='/contact'>
+            Contact
+          </Link>
+          <Link className='rajdhani-bold-24 nav-link' to='/book'>
+            Book
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 };
