0a3c9a7d8a690f90ceb5a267f86e175c04d58b79
Steven Woodward <steveanthony999@gmail.com>
2021-11-28 18:31:43 -0800

    added hero logo and text, styling

--- src/pages/sections/Hero.js
+++ src/pages/sections/Hero.js
@@ -1,6 +1,7 @@
 import './Hero.css';
 
 import HeroImg from '../../assets/hero_img.jpeg';
+import Logo from '../../assets/vlht_logo.png';
 
 const Hero = () => {
   return (
@@ -8,7 +9,18 @@ const Hero = () => {
       className='Hero'
       style={{ background: `url(${HeroImg}) no-repeat center center/cover` }}>
       <div className='overlay'>
-        <h1>Hero Section</h1>
+        <img className='Hero-logo' src={Logo} alt='logo' />
+        <p className='rajdhani-bold-18 text-white'>
+          Venture outside of the{' '}
+          <span className='allura-regular-36'>glittering lights</span> of the
+          Vegas Strip and tour
+          <span>gorgeous homes</span> with scenic views along the way.
+        </p>
+        <p className='rajdhani-bold-18 text-white'>
+          You will love the <span>VIP</span> treatment as you get a taste of the
+          local Vegas
+          <span>luxury lifestyle!</span>
+        </p>
       </div>
     </header>
   );
