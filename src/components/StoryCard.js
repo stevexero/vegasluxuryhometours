8c6ae9d13c32ab9af181776761554f348f12370f
Steven Woodward <steveanthony999@gmail.com>
2021-12-15 23:12:51 -0800

    finished styling story cards for desktop/

--- src/components/StoryCard.js
+++ src/components/StoryCard.js
@@ -1,9 +1,12 @@
 import './StoryCard.css';
 
-const StoryCard = () => {
+const StoryCard = ({ heading, story }) => {
   return (
     <div className='StoryCard'>
-      <h1>Story Card</h1>
+      <div className='StoryCard-container'>
+        <h1 className='cinzel-regular-56 text-dark-silver'>{heading}</h1>
+        <p className='montserrat-regular-14 text-dark-silver mt-32'>{story}</p>
+      </div>
     </div>
   );
 };
--- src/pages/sections/Story.js
+++ src/pages/sections/Story.js
@@ -14,23 +14,38 @@ const Story = () => {
     <div className='Story'>
       <div className='StorySection Story-1'>
         <StoryCardPhoto image={StoryImg_1} />
-        <StoryCard />
+        <StoryCard
+          heading='Ride In Luxury'
+          story='A  luxury vehicle will whisk you away beyond the famous strip!'
+        />
       </div>
       <div className='StorySection Story-2'>
-        <StoryCard />
+        <StoryCard
+          heading='Snack In Style'
+          story='Drinks and snacks will be served as you enjoy a magnificent ride through the surrounding areas that often go unnoticed to tourists.'
+        />
         <StoryCardPhoto image={StoryImg_2} />
       </div>
       <div className='StorySection Story-3'>
         <StoryCardPhoto image={StoryImg_3} />
-        <StoryCard />
+        <StoryCard
+          heading='Lavish Homes'
+          story='Throughout  the tour we will make stops at carefully selected luxury homes and allow you to walk through them to take in the amazing workmanship and creativity'
+        />
       </div>
       <div className='StorySection Story-4'>
-        <StoryCard />
+        <StoryCard
+          heading='Stunning Views'
+          story='Your guide will provide you with interesting information about the various areas and the fabulous homes we tour.'
+        />
         <StoryCardPhoto image={StoryImg_4} />
       </div>
       <div className='StorySection Story-5'>
         <StoryCardPhoto image={StoryImg_5} />
-        <StoryCard />
+        <StoryCard
+          heading='Be a VIP'
+          story="Everyone likes to feel like a VIP and that's my goal! You will  leave with a greater knowledge and appreciation of the lesser known areas and architecture of Las Vegas that are worthy of sightseeing."
+        />
       </div>
     </div>
   );
