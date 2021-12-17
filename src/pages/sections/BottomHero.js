ec0f138b73d9e347f36103d48b8e9aa869599a6b
Steven Woodward <steveanthony999@gmail.com>
2021-12-16 22:06:34 -0800

    styled bottom hero

--- src/pages/sections/BottomHero.js
+++ src/pages/sections/BottomHero.js
@@ -1,9 +1,21 @@
+import { Link } from 'react-router-dom';
+
 import './BottomHero.css';
 
 const BottomHero = () => {
   return (
-    <div className='BottomHero'>
-      <h1>Bottom Hero</h1>
+    <div className='BottomHero ptb-168'>
+      <div className='BottomHero-container container-1100'>
+        <p className='cinzel-regular-56 text-light-silver'>
+          Let us do the research and create a tour of the Las Vegas luxury
+          lifestyle tailored just for you.
+        </p>
+        <Link
+          to='/book'
+          className='SubHero-left-link page-link cinzel-decorative-bold-18 text-dark-gold mt-64'>
+          BOOK YOUR TOUR NOW
+        </Link>
+      </div>
     </div>
   );
 };
