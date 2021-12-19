8162f1163d670498af9dccca77a577fb08d915b2
Steven Woodward <steveanthony999@gmail.com>
2021-12-18 22:26:56 -0800

    laid out footer desktop design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+import IgLogo from '../assets/ig.png';
+
 import './Footer.css';
 
 const Footer = () => {
@@ -23,16 +25,36 @@ const Footer = () => {
       <div className='Footer-body ptb-84'>
         <div className='Footer-body-left'>
           <h3 className='rajdhani-bold-18 text-white'>FOLLOW US!</h3>
+          <a
+            href='https://www.instagram.com/lasvegasluxuryhometours/'
+            target='_blank'
+            rel='noreferrer'
+            className='mt-16'>
+            <img src={IgLogo} alt='instagram' width='29px' />
+          </a>
         </div>
         <div className='Footer-body-center'>
           <h3 className='rajdhani-bold-18 text-white'>COMPANY</h3>
+          <Link to='/about' className='montserrat-light-14 text-white mt-16'>
+            About
+          </Link>
         </div>
         <div className='Footer-body-right'>
           <h3 className='rajdhani-bold-18 text-white'>SUPPORT</h3>
-          <Link to='/attributions'>Attributions</Link>
+          <Link to='/contact' className='montserrat-light-14 text-white mt-16'>
+            Contact
+          </Link>
+          <br />
+          <Link to='/attributions' className='montserrat-light-14 text-white'>
+            Attributions
+          </Link>
         </div>
       </div>
-      <div className='Footer-foot'>foot</div>
+      <div className='Footer-foot'>
+        <p className='mt-32 montserrat-light-14 text-white'>
+          COPYRIGHT &copy; 2021 - VEGAS LUXURY HOME TOURS
+        </p>
+      </div>
     </footer>
   );
 };
