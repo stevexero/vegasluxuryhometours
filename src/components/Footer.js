434e783f7ee3e93508b6071ec55cc1c7afe1d491
Steven Woodward <steveanthony999@gmail.com>
2021-12-18 17:41:19 -0800

    styled footer inputs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 import './Footer.css';
 
@@ -7,7 +8,17 @@ const Footer = () => {
     <footer className='Footer ptb-84'>
       <div className='Footer-head'>
         <input type='text' placeholder='JOIN OUR MAILING LIST' />
-        <button className='btn-footer'>SUBSCRIBE</button>
+        <motion.button
+          whileHover={{
+            scale: 1.1,
+            transition: {
+              duration: 0.2,
+              type: 'spring',
+            },
+          }}
+          className='btn-footer alfa-slab-one-18'>
+          SUBSCRIBE
+        </motion.button>
       </div>
       <div className='Footer-body ptb-84'>
         <Link to='/attributions'>Attributions</Link>
