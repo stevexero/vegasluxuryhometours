d4294ccf89d9daaafab59fea0b7b146df7b05582
Steven Woodward <steveanthony999@gmail.com>
2021-12-21 21:36:41 -0800

    working on nav responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,61 +1,75 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 
 import Logo from '../assets/vlht_logo.png';
 
 import './Navbar.css';
 
 const Navbar = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 414px)' });
+
   const [logoVisibility, setLogoVisibility] = useState('hidden');
   const [navBackgroundColor, setNavBackgroundColor] = useState('transparent');
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   const handleScroll = (e) => {
-    let scrollTop = window.scrollY;
-
-    if (scrollTop > 298) {
-      setLogoVisibility('visible');
-      setNavBackgroundColor('black');
+    if (isMobile) {
+      // Do Nothing
     } else {
-      setLogoVisibility('hidden');
-      setNavBackgroundColor('transparent');
+      let scrollTop = window.scrollY;
+
+      if (scrollTop > 298) {
+        setLogoVisibility('visible');
+        setNavBackgroundColor('black');
+      } else {
+        setLogoVisibility('hidden');
+        setNavBackgroundColor('transparent');
+      }
     }
   };
+
   return (
     <nav
       className='Navbar'
       style={{ background: navBackgroundColor, transition: '0.3s' }}>
-      <div className='Navbar-container'>
-        <div className='Navbar-left'>
-          <Link className='rajdhani-bold-24 nav-link' to='/'>
-            Home
-          </Link>
-          <Link className='rajdhani-bold-24 nav-link' to='/about'>
-            About
-          </Link>
+      {isMobile ? (
+        <div>mNav</div>
+      ) : (
+        <div className='Navbar-container'>
+          <div className='Navbar-left'>
+            <Link className='rajdhani-bold-24 nav-link' to='/'>
+              Home
+            </Link>
+            <Link className='rajdhani-bold-24 nav-link' to='/about'>
+              About
+            </Link>
+          </div>
+          <img
+            className='Hero-logo'
+            src={Logo}
+            alt='logo'
+            style={{
+              width: '130px',
+              visibility: logoVisibility,
+              transition: logoVisibility === 'hidden' ? '0.01s' : '0s',
+            }}
+          />
+          <div className='Navbar-right'>
+            <Link className='rajdhani-bold-24 nav-link' to='/contact'>
+              Contact
+            </Link>
+            <Link className='rajdhani-bold-24 nav-link' to='/book'>
+              Book
+            </Link>
+          </div>
         </div>
-        <img
-          className='Hero-logo'
-          src={Logo}
-          alt='logo'
-          style={{
-            width: '130px',
-            visibility: logoVisibility,
-            transition: logoVisibility === 'hidden' ? '0.01s' : '0s',
-          }}
-        />
-        <div className='Navbar-right'>
-          <Link className='rajdhani-bold-24 nav-link' to='/contact'>
-            Contact
-          </Link>
-          <Link className='rajdhani-bold-24 nav-link' to='/book'>
-            Book
-          </Link>
-        </div>
-      </div>
+      )}
     </nav>
   );
 };
