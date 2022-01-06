a0a3703b07afaae879e4667dc8de2d9b3302c9a3
Steven Woodward <steveanthony999@gmail.com>
2022-01-05 21:46:29 -0800

    set up booking section

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -1,7 +1,13 @@
 import { useEffect } from 'react';
+import { useMediaQuery } from 'react-responsive';
+
 import './Book.css';
 
 const Book = () => {
+  const isMobileSmall = useMediaQuery({ query: '(max-width: 320px)' });
+  const isMobile = useMediaQuery({ query: '(max-width: 414px)' });
+  const isTabletSmall = useMediaQuery({ query: '(max-width: 768px)' });
+
   useEffect(() => {
     const script = document.createElement('script');
 
@@ -14,20 +20,36 @@ const Book = () => {
   return (
     <div className='Book'>
       <div className='Book-container'>
-        <div
-          className='airbnb-embed-frame'
-          data-id='2649345'
-          data-view='experience_booking'
-          data-image='true'
-          data-title_and_info='true'
-          data-currency='USD'>
-          <a href='https://www.airbnb.com/experiences/2649345?source=booking_widget'>
-            View On Airbnb
-          </a>
-          <a
-            href='https://www.airbnb.com/experiences/2649345?source=booking_widget'
-            rel='nofollow'></a>
-        </div>
+        {isTabletSmall ? (
+          <div
+            class='airbnb-embed-frame'
+            data-id='2649345'
+            data-view='experience_booking'
+            data-vertical='true'
+            data-currency='USD'>
+            <a href='https://www.airbnb.com/experiences/2649345?source=booking_widget'>
+              View On Airbnb
+            </a>
+            <a
+              href='https://www.airbnb.com/experiences/2649345?source=booking_widget'
+              rel='nofollow'></a>
+          </div>
+        ) : (
+          <div
+            className='airbnb-embed-frame'
+            data-id='2649345'
+            data-view='experience_booking'
+            data-image='true'
+            data-title_and_info='true'
+            data-currency='USD'>
+            <a href='https://www.airbnb.com/experiences/2649345?source=booking_widget'>
+              View On Airbnb
+            </a>
+            <a
+              href='https://www.airbnb.com/experiences/2649345?source=booking_widget'
+              rel='nofollow'></a>
+          </div>
+        )}
       </div>
     </div>
   );
