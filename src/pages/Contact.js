a2d4a8e97b3eddee76837478e827ce209f341ce2
Steven Woodward <steveanthony999@gmail.com>
2022-01-29 23:24:20 -0800

    added number, email, and styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -36,10 +36,14 @@ const Contact = () => {
       }}
       className='Contact'>
       <div className='Contact-container'>
-        <div className='Contact-top'>
+        <div className='Contact-top mt-64'>
           <h1 className='cinzel-regular-56 text-dark-silver'>Contact Us</h1>
+          <h4 className='text-dark-silver mt-32'>
+            Fill out the form below, call, or send an email with questions,
+            comments, concerns, or to set up a custom tour.
+          </h4>
         </div>
-        <div className='Contact-bottom'>
+        <div className='Contact-bottom mt-32'>
           <div className='Contact-left'>
             <form onSubmit={sendEmail}>
               <input type='text' name='name' required placeholder='Name' />
@@ -52,7 +56,22 @@ const Contact = () => {
               <button className='alfa-slab-one-18'>SEND</button>
             </form>
           </div>
-          <div className='Contact-right'>Phone number, email</div>
+          <div className='Contact-right'>
+            <div className='Contact-right-top'>
+              <h3 className='cinzel-regular-30 text-dark-silver'>Call</h3>
+              <a href='tel:+17024177839' className='cinzel-regular-24'>
+                (702) 417-7839
+              </a>
+            </div>
+            <div className='Contact-right-bottom'>
+              <h3 className='cinzel-regular-30 text-dark-silver'>email</h3>
+              <a
+                href='mailto:vegasluxuryhometours@aol.com'
+                className='cinzel-regular-24'>
+                vegasluxuryhometours@aol.com
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </motion.div>
