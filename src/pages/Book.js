d73854661a027fefc434bcd80d26c58a1640b3f7
Steven Woodward <steveanthony999@gmail.com>
2022-01-30 00:20:57 -0800

    migrated calendly

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useMediaQuery } from 'react-responsive';
+// import { useMediaQuery } from 'react-responsive';
 import { motion } from 'framer-motion';
 
 import './Book.css';
@@ -7,12 +7,11 @@ import './Book.css';
 const Book = () => {
   // const isMobileSmall = useMediaQuery({ query: '(max-width: 320px)' });
   // const isMobile = useMediaQuery({ query: '(max-width: 414px)' });
-  const isTabletSmall = useMediaQuery({ query: '(max-width: 768px)' });
+  // const isTabletSmall = useMediaQuery({ query: '(max-width: 768px)' });
 
   useEffect(() => {
     const script = document.createElement('script');
 
-    // script.src = 'https://www.airbnb.com/embeddable/airbnb_jssdk';
     script.src = 'https://assets.calendly.com/assets/external/widget.js';
     script.async = true;
 
@@ -28,48 +27,12 @@ const Book = () => {
       exit={{
         opacity: 0,
         transition: { duration: 0.5 },
-      }}
-    >
+      }}>
       <div className='Book-container'>
-        {/* {isTabletSmall ? (
-          <div
-            className='airbnb-embed-frame'
-            data-id='2649345'
-            data-view='experience_booking'
-            data-vertical='true'
-            data-currency='USD'>
-            <a href='https://www.airbnb.com/experiences/2649345?source=booking_widget'>
-              View On Airbnb
-            </a>
-            <a
-              href='https://www.airbnb.com/experiences/2649345?source=booking_widget'
-              rel='nofollow'>
-              {''}
-            </a>
-          </div>
-        ) : (
-          <div
-            className='airbnb-embed-frame'
-            data-id='2649345'
-            data-view='experience_booking'
-            data-image='true'
-            data-title_and_info='true'
-            data-currency='USD'>
-            <a href='https://www.airbnb.com/experiences/2649345?source=booking_widget'>
-              View On Airbnb
-            </a>
-            <a
-              href='https://www.airbnb.com/experiences/2649345?source=booking_widget'
-              rel='nofollow'>
-              {''}
-            </a>
-          </div>
-        )} */}
         <div
           className='calendly-inline-widget'
-          data-url='https://calendly.com/redrunedigital?background_color=ffffff&text_color=606167&primary_color=0000ff'
-          style={{ minWidth: '320px', height: '1130px' }}
-        ></div>
+          data-url='https://calendly.com/vegasluxuryhometours?background_color=ffffff&text_color=606167&primary_color=0000ff'
+          style={{ minWidth: '320px', height: '100%' }}></div>
       </div>
     </motion.div>
   );
