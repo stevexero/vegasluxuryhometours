edb6be25ebd0b16fadf3c1865e6107ff360c4bf0
Steven Woodward <steveanthony999@gmail.com>
2022-02-04 10:59:55 -0800

    styling blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import BlockContent from '@sanity/block-content-to-react';
 
 import sanityClient from '../client';
 
@@ -25,35 +26,44 @@ const Blog = () => {
         }`
       )
       .then((data) => setPostData(data))
-      //   .then((data) => console.log(data))
       .catch(console.error);
   }, []);
 
   return (
     <div className='Blog'>
-      <h1>Blog</h1>
-      <h2>Welcome to the Vegas Luxury Home Tours Blog</h2>
-      <div className='Blog-container'>
-        {postData &&
-          postData.map((post, index) => (
-            <article key={index} className='Blog-card'>
-              <Link to={'/blog/' + post.slug.current} key={post.slug.current}>
-                <span key={index}>
-                  <div
-                    className='Blog-image'
-                    style={{
-                      background: `url(${post.mainImage.asset.url}) no-repeat center center/cover`,
-                      width: '100%',
-                      height: '200px',
-                    }}
-                  ></div>
-                  <span>
-                    <h3>{post.title}</h3>
+      <div className='Blog-header'>
+        <h1>Welcome to the Vegas Luxury Home Tours Blog</h1>
+      </div>
+      <div className='Blog-body'>
+        <div className='Blog-container'>
+          {postData &&
+            postData.map((post, index) => (
+              <article key={index} className='Blog-card'>
+                <Link to={'/blog/' + post.slug.current} key={post.slug.current}>
+                  <span key={index}>
+                    <div
+                      className='Blog-image'
+                      style={{
+                        background: `url(${post.mainImage.asset.url}) no-repeat center center/cover`,
+                        width: '100%',
+                        height: '200px',
+                      }}
+                    ></div>
+                    <span>
+                      <h3>{post.title}</h3>
+                    </span>
+                    <span className='Blog-content'>
+                      <BlockContent
+                        blocks={post.body}
+                        projectId='8rk8cdbm'
+                        dataset='production'
+                      />
+                    </span>
                   </span>
-                </span>
-              </Link>
-            </article>
-          ))}
+                </Link>
+              </article>
+            ))}
+        </div>
       </div>
     </div>
   );
