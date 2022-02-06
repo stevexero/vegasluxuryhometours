08029dd28ff547664bb87279c8d5cc5b4e06de75
Steven Woodward <steveanthony999@gmail.com>
2022-02-05 22:53:41 -0800

    styling blog

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import sanityClient from '../client';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar, faUser } from '@fortawesome/free-solid-svg-icons';
 import imageUrlBuilder from '@sanity/image-url';
 import BlockContent from '@sanity/block-content-to-react';
+
+import sanityClient from '../client';
 
 import './SinglePost.css';
 
@@ -49,28 +52,26 @@ const SinglePost = () => {
         <h1 className='cinzel-bold-56 text-dark-silver text-center'>
           {singlePost.title.toUpperCase()}
         </h1>
-        <Link to='/about' className='SinglePost-author'>
+        <Link to='/about' className='SinglePost-author text-dark-silver mt-16'>
           <div className='SinglePost-author-left'>
-            <img
-              src={urlFor(singlePost.authorImage).width(50).url()}
-              alt={singlePost.name}
-              className='SinglePost-author-image'
-            />
-            <p>{singlePost.name}</p>
+            <FontAwesomeIcon icon={faUser} />
+            <p className='montserrat-light-12 ml-16'>{singlePost.name}</p>
           </div>
-          <div className='SinglePost-author-right'>
-            <p>{new Date(singlePost.publishedAt).toDateString()}</p>
+          <div className='SinglePost-author-right text-dark-silver'>
+            <FontAwesomeIcon icon={faCalendar} className='ml-32' />
+            <p className='montserrat-light-12 ml-16'>
+              {new Date(singlePost.publishedAt).toDateString()}
+            </p>
           </div>
         </Link>
         <div
           className='SinglePost-image mt-64'
           style={{
             background: `url(${singlePost.mainImage.asset.url})`,
-          }}
-        ></div>
+          }}></div>
       </div>
 
-      <div className='SinglePost-body'>
+      <div className='SinglePost-body mb-64'>
         <BlockContent
           blocks={singlePost.body}
           projectId='8rk8cdbm'
