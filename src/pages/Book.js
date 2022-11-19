d5756dc7e5c2b060fec431f67dd68ed25e2b2282
Steven Woodward <steveanthony999@gmail.com>
2022-11-18 20:42:16 -0800

    added pricing

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -27,12 +27,44 @@ const Book = () => {
       exit={{
         opacity: 0,
         transition: { duration: 0.5 },
-      }}>
+      }}
+    >
       <div className='Book-container'>
+        <div className='price-container'>
+          <h1>Pricing</h1>
+          <hr />
+          <div className='price-table'>
+            <div className='price-table-head'>
+              <h2>Individual prices</h2>
+            </div>
+            <div className='price-table-body'>
+              <p>Adult</p>
+              <p>from $74</p>
+            </div>
+            <div className='price-table-body'>
+              <p>Child (ages 4-12)</p>
+              <p>from $68</p>
+            </div>
+          </div>
+          <hr />
+          <div className='price-table'>
+            <div className='price-table-head'>
+              <h2>Private Group</h2>
+            </div>
+            <div className='price-table-body'>
+              <p>Up to 4 guests</p>
+              <p>from $74</p>
+            </div>
+            <p className='small-text'>
+              Final price will be determined by the number of guests.
+            </p>
+          </div>
+        </div>
         <div
           className='calendly-inline-widget'
           data-url='https://calendly.com/vegasluxuryhometours?background_color=ffffff&text_color=606167&primary_color=0000ff'
-          style={{ minWidth: '320px', height: '100%' }}></div>
+          style={{ minWidth: '320px', height: '100%' }}
+        ></div>
       </div>
     </motion.div>
   );
